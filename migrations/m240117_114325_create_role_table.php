d672e40eb7d6343ad90b1a312faf9ba883750462
Грушевский Юрий <jurapro@yandex.ru>
2024-01-17 04:01:44 -0800

    Разработана база данных

---
+++ migrations/m240117_114325_create_role_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%role}}`.
+ */
+class m240117_114325_create_role_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%role}}', [
+            'id' => $this->primaryKey(),
+            'code' => $this->string()->notNull()->unique(),
+            'name' => $this->string()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%role}}');
+    }
+}
---
+++ migrations/m240117_115159_create_user_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ * Has foreign keys to the tables:
+ *
+ * - `{{%role}}`
+ */
+class m240117_115159_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'role_id' => $this->integer()->defaultValue(1),
+            'username' => $this->string()->notNull()->unique(),
+            'password' => $this->string()->notNull(),
+            'email' => $this->string()->notNull()->unique(),
+            'first_name' => $this->string()->notNull(),
+            'last_name' => $this->string()->notNull(),
+            'middle_name' => $this->string()->notNull(),
+        ]);
+
+        // creates index for column `role_id`
+        $this->createIndex(
+            '{{%idx-user-role_id}}',
+            '{{%user}}',
+            'role_id'
+        );
+
+        // add foreign key for table `{{%role}}`
+        $this->addForeignKey(
+            '{{%fk-user-role_id}}',
+            '{{%user}}',
+            'role_id',
+            '{{%role}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%role}}`
+        $this->dropForeignKey(
+            '{{%fk-user-role_id}}',
+            '{{%user}}'
+        );
+
+        // drops index for column `role_id`
+        $this->dropIndex(
+            '{{%idx-user-role_id}}',
+            '{{%user}}'
+        );
+
+        $this->dropTable('{{%user}}');
+    }
+}
